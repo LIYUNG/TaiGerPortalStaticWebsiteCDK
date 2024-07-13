2f2b5155f1d721a8fd23bf5c3e4dbb993e59bd6d
Leo <liyung.chen@tum.de>
2024-07-14 01:47:37 +0200

    invalidate cloudfront and add iam policy

--- lib/taiger_portal_cdk-stack.ts
+++ lib/taiger_portal_cdk-stack.ts
@@ -69,13 +69,17 @@ export class TaiGerPortalCdkStack extends Stack {
             },
           },
           pre_build: {
-            commands: ['npm install'],
+            commands: [
+              // 'cd client', // Navigate to the subdirectory
+              'npm install',
+            ],
           },
           build: {
             commands: ['npm run build'],
           },
         },
         artifacts: {
+          // 'base-directory': 'client/build', // Adjust base directory to the subdirectory's build folder
           'base-directory': 'build', // specify the base directory where build artifacts are located
           files: ['**/*'], // include all files and subdirectories under 'build'
         },
@@ -89,6 +93,43 @@ export class TaiGerPortalCdkStack extends Stack {
       input: sourceOutput,
       outputs: [new codepipeline.Artifact()],
     });
+
+    // CodeBuild project for CloudFront cache invalidation
+    const invalidateCacheProject = new codebuild.PipelineProject(
+      this,
+      'InvalidateCacheProject',
+      {
+        environment: {
+          buildImage: codebuild.LinuxBuildImage.AMAZON_LINUX_2_4,
+        },
+        buildSpec: codebuild.BuildSpec.fromObject({
+          version: '0.2',
+          phases: {
+            build: {
+              commands: [
+                // Replace with AWS CLI command to invalidate CloudFront cache
+                'aws cloudfront create-invalidation --distribution-id E140WBRXPYSUB4 --paths "/*"',
+              ],
+            },
+          },
+        }),
+      }
+    );
+    // Add the necessary permissions to the CodeBuild project's role
+    invalidateCacheProject.addToRolePolicy(
+      new iam.PolicyStatement({
+        actions: ['cloudfront:CreateInvalidation'],
+        resources: [
+          `arn:aws:cloudfront::${props?.env?.account}:distribution/E140WBRXPYSUB4`,
+        ],
+      })
+    );
+    const invalidationCloudfrontAction =
+      new codepipeline_actions.CodeBuildAction({
+        actionName: 'Invalidate_Cache',
+        project: invalidateCacheProject,
+        input: sourceOutput,
+      });
 
     // Deploy to Beta
     const betaDeployAction = new codepipeline_actions.S3DeployAction({
@@ -117,7 +158,7 @@ export class TaiGerPortalCdkStack extends Stack {
         },
         {
           stageName: 'Beta_Deploy',
-          actions: [betaDeployAction],
+          actions: [betaDeployAction, invalidationCloudfrontAction],
         },
         // {
         //   stageName: 'Prod_Deploy',
