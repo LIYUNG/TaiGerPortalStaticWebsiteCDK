997a94164f6cc416945e55d1f7a52909476371da
Leo <liyung.chen@tum.de>
2024-07-13 22:40:50 +0200

    add constants, test simple SQS creation

--- bin/taiger_portal_cdk.ts
+++ bin/taiger_portal_cdk.ts
@@ -2,9 +2,10 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { TaiGerPortalCdkStack } from '../lib/taiger_portal_cdk-stack';
+import { Region, Stage } from '../constants';
 
 const app = new cdk.App();
-new TaiGerPortalCdkStack(app, 'TaiGerPortalCdkStack', {
+new TaiGerPortalCdkStack(app, `TaiGerPortalCdkStack-${Stage.Beta_FE}`, {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -15,7 +16,7 @@ new TaiGerPortalCdkStack(app, 'TaiGerPortalCdkStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  env: { account: '669131042313', region: Region.IAD },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
---
+++ constants/stages.ts
@@ -0,0 +1,4 @@
+export enum Stage {
+  Beta_FE = 'Beta-FE',
+  Prod_NA = 'Prod-NA',
+}
