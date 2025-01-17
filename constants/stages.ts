935b803b2ade30bee4f1d4fc0b268b51d6aff0bb
Leo <liyung.chen@tum.de>
2025-01-17 09:28:10 +0100

    remove prod stage at the moment

--- constants/stages.ts
+++ constants/stages.ts
@@ -2,8 +2,8 @@ import {
     API_BETA_DOMAIN,
     API_PROD_DOMAIN,
     AWS_ACCOUNT,
-    AWS_S3_BUCKET_DEV_FRONTEND,
-    AWS_S3_BUCKET_PROD_FRONTEND,
+    // AWS_S3_BUCKET_DEV_FRONTEND,
+    // AWS_S3_BUCKET_PROD_FRONTEND,
     STATIC_ASSETS_BUCKET_DEV,
     STATIC_ASSETS_BUCKET_PROD
 } from "../configuration";
@@ -18,17 +18,15 @@ export const STAGES = [
     {
         stageName: Stage.Beta_FE,
         env: { region: Region.IAD, account: AWS_ACCOUNT },
-        bucketArn: AWS_S3_BUCKET_DEV_FRONTEND,
         staticAssetsBucketName: STATIC_ASSETS_BUCKET_DEV,
         isProd: false,
         apiDomain: API_BETA_DOMAIN
-    },
-    {
-        stageName: Stage.Prod_NA,
-        env: { region: Region.NRT, account: AWS_ACCOUNT },
-        bucketArn: AWS_S3_BUCKET_PROD_FRONTEND,
-        staticAssetsBucketName: STATIC_ASSETS_BUCKET_PROD,
-        isProd: true,
-        apiDomain: API_PROD_DOMAIN
     }
+    // {
+    //     stageName: Stage.Prod_NA,
+    //     env: { region: Region.NRT, account: AWS_ACCOUNT },
+    //     staticAssetsBucketName: STATIC_ASSETS_BUCKET_PROD,
+    //     isProd: true,
+    //     apiDomain: API_PROD_DOMAIN
+    // }
 ];
